02a4eec50a2c021e5d5bbde4043b9c30cd11118d
Jacob Esteves <esteves.jacob.c@gmail.com>
2024-12-05 15:42:18 -0500

    some fixes

--- csvToDbTableInserts.sql
+++ csvToDbTableInserts.sql
@@ -1,3 +1,4 @@
+-- Olympic Swimming History Table
 CREATE TABLE olympicSwimmingResults_1912_2020 (
     id INT PRIMARY KEY,
     Location VARCHAR(50) NOT NULL,
@@ -12,9 +13,14 @@ CREATE TABLE olympicSwimmingResults_1912_2020 (
     Rank INT NOT NULL
 );
 
--- Example insertions of values from csv dile
+-- Example insertions of values from csv file
 INSERT INTO olympicSwimmingResults_1912_2020 VALUES(1, "Tokyo", 2020, "100m", "Backstroke", 0, "Men", "ROC", "Evgeny Rylov", "51.98", 1);
 INSERT INTO olympicSwimmingResults_1912_2020 VALUES(2, "Tokyo", 2020, "100m", "Backstroke", 0, "Men", "ROC", "Kliment Kolesnikov", "52", 2);
 INSERT INTO olympicSwimmingResults_1912_2020 VALUES(3, "Tokyo", 2020, "100m", "Backstroke", 0, "Men", "USA", "Ryan Murphy", "52.19", 3);
 INSERT INTO olympicSwimmingResults_1912_2020 VALUES(4, "Tokyo", 2020, "100m", "Backstroke", 0, "Men", "ITA", "Thomas Ceccon", "52.3", 4);
 INSERT INTO olympicSwimmingResults_1912_2020 VALUES(5, "Tokyo", 2020, "100m", "Backstroke", 0, "Men", "CHN", "Jiayu Xu", "52.51", 4);
+
+-- Get the column types and corresponding data types
+SELECT COLUMN_NAME, DATA_TYPE
+    FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = "olympicSwimmingResults_1912_2020";
